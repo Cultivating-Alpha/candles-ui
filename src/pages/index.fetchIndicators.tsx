b6f1e931e28833b2e097de44a72bbcda1bb6ed14
Alpha <piotr@piotryordanov.com>
2023-08-24 12:04:02 +0400

    feat: formatting

--- src/pages/index.fetchIndicators.tsx
+++ src/pages/index.fetchIndicators.tsx
@@ -1,40 +1,33 @@
-import axios from "axios";
-import { usePapaParse } from "react-papaparse";
-import * as echarts from "echarts";
+import axios from 'axios';
+import { usePapaParse } from 'react-papaparse';
 
 const fetchIndicators = (setState) => {
   const { readString } = usePapaParse();
-  axios
-    .get("/indicators.json")
-    .then((res) => {
-      const settings = res.data
-      const series = []
-      axios
-        .get("/indicators.csv")
-        .then((res) => {
-          readString(res.data, {
-            worker: true,
-            dynamicTyping: true,
-            complete: (results) => {
-               settings.map((setting) => {
-                 series.push({
-                   ...setting,
-                   data: results.data.map(arr => arr[setting.dataIndex]),
-                  xAxisIndex: setting.panel,
-                  yAxisIndex: setting.panel,
-                 })
-               })
+  axios.get('/indicators.json').then((res) => {
+    const settings = res.data;
+    const series = [];
+    axios.get('/indicators.csv').then((res) => {
+      readString(res.data, {
+        worker: true,
+        dynamicTyping: true,
+        complete: (results) => {
+          settings.map((setting) => {
+            series.push({
+              ...setting,
+              data: results.data.map((arr) => arr[setting.dataIndex]),
+              xAxisIndex: setting.panel,
+              yAxisIndex: setting.panel,
+            });
+          });
 
-              setState(prevState => ({
-                ...prevState,
-                indicators: series,
-              }));
-            },
-          })
-        })
+          setState((prevState) => ({
+            ...prevState,
+            indicators: series,
+          }));
+        },
+      });
+    });
+  });
+};
 
-    })
-
-}
-
-export default fetchIndicators
+export default fetchIndicators;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from "react";
-import ReactECharts from "echarts-for-react"; // or var ReactECharts = require('echarts-for-react');
+import ReactECharts from 'echarts-for-react'; // or var ReactECharts = require('echarts-for-react');
+import React, { useEffect, useState } from 'react';
 
-import fetchData from "./index.fetchData";
-import fetchIndicators from "./index.fetchIndicators";
-import generateOption from "./index.generateOption";
-
+import fetchData from './index.fetchData';
+import fetchIndicators from './index.fetchIndicators';
+import generateOption from './index.generateOption';
 
 interface State {
   height: number;
   equityData: number[]; // Replace 'any' with the actual type of equityData
-  indData: number[];   // Replace 'any' with the actual type of indData
-  dates: number[];     // Replace 'any' with the actual type of dates
+  indData: number[]; // Replace 'any' with the actual type of indData
+  dates: number[]; // Replace 'any' with the actual type of dates
   candleData: number[];
   markData: number[];
   scatterData: number[];
@@ -23,36 +22,42 @@ const defaultState: State = {
   candleData: [],
   markData: [],
   scatterData: [],
-  indicators: [] 
-}
+  indicators: [],
+};
 
-
-export default () => {
+const Index = () => {
   const [state, setState] = useState(defaultState);
 
   useEffect(() => {
     if (state.height === 0) {
-      setState(prevState => ({
+      setState((prevState) => ({
         ...prevState,
-        height: window.innerHeight - 50
+        height: window.innerHeight - 5,
       }));
-      fetchData(setState)
-      fetchIndicators(setState)
+      fetchData(setState);
+      fetchIndicators(setState);
     }
   }, [state]);
 
   if (state.height === 0) {
-    return <div className=""></div>;
+    return <div className="" />;
   }
 
   if (state.dates.length > 0) {
     return (
       <div className="min-h-screen">
-        <ReactECharts style={{ height: state.height, width: "100%" }} option={generateOption(state)} notMerge={true} lazyUpdate={true} theme={"theme_name"} />
+        <ReactECharts
+          style={{ height: state.height, width: '100%' }}
+          option={generateOption(state)}
+          notMerge
+          lazyUpdate
+          theme="theme_name"
+        />
       </div>
     );
   }
 
-  return <div className=""></div>;
-
+  return <div className="" />;
 };
+
+export default Index;
